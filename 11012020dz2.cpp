ef530380ca96392d6e72e2a3e409ce6b89637cfb
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:04:45 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz2.cpp
@@ -0,0 +1,15 @@
+﻿//* Пользователь 	вводит с клавиатуры расстояние до 	аэропорта и время, за которое нужно 	доехать.Вычислить скорость, с которой 	ему нужно ехать.
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int rast, h, m;
+	cout << "Введите растояние (в км.): ";
+	cin >> rast;
+	cout << "Введите время (в часах, затем в минутах): ";
+	cin >> h >> m;
+	cout << rast / double(h + (m / 60)) << " км/час" << endl;
+}
